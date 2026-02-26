8288ff2ea53d1f816fe85de153d0e55190ef06ff
Daria Lasecka <daria.lasecka@htdevelopers.com>
2026-02-26 12:51:27 +0100

    Update host config to use env variables

--- Host/vite.config.ts
+++ Host/vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    target: "esnext",
-  },
-  plugins: [
-    react(),
-    federation({
-      name: "app",
-      remotes: {
-        remote_app: "http://localhost:5001/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-  server: {
-    port: 5000,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    build: {
+      target: "esnext",
+    },
+    plugins: [
+      react(),
+      federation({
+        name: "app",
+        remotes: {
+          remote_app: `${env.VITE_REMOTE_URL}/assets/remoteEntry.js`,
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    server: {
+      port: 5000,
+    },
+  };
 });
